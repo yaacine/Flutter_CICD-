88d76577ba95acac5759b2acbe555d819821a0a6
yaacine <gy_zidelmal@esi.dz>
2020-09-10 20:15:06 +0100

    refactor(formating): refactoring of the code and push on pr

--- test/flutter_cicd_test.dart
+++ test/flutter_cicd_test.dart
@@ -7,11 +7,8 @@ void main() {
     final calculator = Calculator();
 
     expect(calculator.addOne(2), 3);
-
     expect(calculator.addOne(-7), -6);
-
     expect(calculator.addOne(0), 1);
-
     expect(() => calculator.addOne(null), throwsNoSuchMethodError);
   });
 }
