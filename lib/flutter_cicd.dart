6ecae657c2f6bbdbf757e79847550a5ec505e293
yaacine <gy_zidelmal@esi.dz>
2020-09-11 11:16:11 +0100

    refactor(formating): refactoring of the code for lint test with null values

--- lib/flutter_cicd.dart
+++ lib/flutter_cicd.dart
@@ -3,7 +3,7 @@ library flutter_cicd;
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
-
+  dynamic _test = "";
   int addOne(int value) => value + 1;
-  int t = 0;
+  int t = null;
 }
